3ba89f91c06a942fff47e5b455967f1cfc2ac569
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-21 17:21:03 -0600

    Add uuid, create post method Co-authored-by: Bridgett Coyle <bacoyle0409@gmail.com> Co-authored-by: Adam Niedzwiecki <AdamN8142@gmail.com> Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/api/v1/notes', (request, response) => {
   return response.status(200).json({ notes })
 });
 
+app.use(express.json());
+
+app.post('/api/v1/notes', (request, reesponse) => {
+  
+})
+
 app.listen(app.get('port'), () => {
   console.log('something running in 3000');
 })
