a74fcdf0cdf519a0ac4d4f003c1bddb9fb0de236
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-21 17:16:26 -0600

    Create root get request response method Co-authored-by: Bridgett Coyle <bacoyle0409@gmail.com> Co-authored-by: Adam Niedzwiecki <AdamN8142@gmail.com> Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>

--- server.js
+++ server.js
@@ -2,5 +2,15 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.locals.notes = [];
+app.locals.notes = [
+  {note: 'randomnote'}
+];
 
+app.get('/', (request, response) => {
+  const notes = app.locals.notes
+  return response.status(200).json({ notes })
+});
+
+app.listen(app.get('port'), () => {
+  console.log('something running in 3000');
+})
