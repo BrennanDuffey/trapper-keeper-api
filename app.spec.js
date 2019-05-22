47558bffa8a8551e66f5c5cb017d36d8215266b3
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-22 14:31:59 -0600

    Add describe API block skeleton to testing file
    Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>'

--- app.spec.js
+++ app.spec.js
@@ -1,3 +1,23 @@
-import request from 'supertest';
-import '@babel/polyfill';
-import app from './app';
+import request from "supertest";
+import "@babel/polyfill";
+import app from "./app";
+
+describe("API", () => {
+  let notes;
+  beforeEach(() => {
+    notes = [
+      {
+        title: "randomnote",
+        id: "1",
+        listItems: [{ id: "1", body: "asdf", completed: false }]
+      },
+      {
+        title: "randomnoteTWO",
+        id: "2",
+        listItems: [{ id: "2", body: "asdf", completed: false }]
+      }
+    ];
+
+    app.locals.notes
+  });
+});
