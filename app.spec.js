0c8a5b1f8f8eacf51dd461a408ff5f285372178c
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-22 14:38:44 -0600

    Add several passing tests for GET
    Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>

--- app.spec.js
+++ app.spec.js
@@ -17,7 +17,30 @@ describe("API", () => {
         listItems: [{ id: "2", body: "asdf", completed: false }]
       }
     ];
-
-    app.locals.notes
+    app.locals.notes = notes
   });
+  describe("GET /api/v1/notes", () =>{
+    it('should have a status of 200', async() => {
+        const response = await request(app).get("/api/v1/notes");
+        expect(response.statusCode).toBe(200);
+    });
+
+    it('should return an array of notes', async () => {
+        const response = await request(app).get("/api/v1/notes");
+        expect(response.body.notes).toEqual(notes)
+    });
+  })
+
+  describe("GET /api/v1/notes/:id", () => {
+      it('should return a status of 200', async()=> {
+        const response = await request(app).get("/api/v1/notes/1");
+        expect(response.statusCode).toBe(200);
+
+      });
+
+      it('should return a note', async() => {
+        const response = await request(app).get("/api/v1/notes/1");
+        expect(response.body).toEqual(notes[0])
+      })
+  })
 });
